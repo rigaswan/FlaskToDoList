b084d5515b35194ded8b2a0c8724cbd576d44d59
rigaswan <rigas.wan@gmail.com>
2023-10-31 12:34:23 +0800

    database testing

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1> hello world</h1>"
+
+@app.route("/dbtest")
+def detest():
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    cursor.execute("select * from user")
+    data = cursor.fetchall()
+    return render_template("db.html",data=data)
